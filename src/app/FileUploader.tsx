b763e1e7cfca238923fa673a7f153aae51902277
makatadi <38482112+draJiang@users.noreply.github.com>
2024-03-19 03:49:48 +0800

    增加赞助入口

--- src/app/FileUploader.tsx
+++ src/app/FileUploader.tsx
@@ -171,7 +171,7 @@ function FileUploader() {
                     <div className="sm:flex sm:items-center sm:justify-between">
                         {/* <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://notes.dabing.one" className="hover:underline">Jiang</a>. All Rights Reserved.
                         </span> */}
-                        <div className="flex mt-4 sm:justify-center sm:mt-0">
+                        <div className="flex mt-4 sm:justify-center items-center sm:mt-0">
                             {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                                     <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
@@ -195,6 +195,11 @@ function FileUploader() {
                                     <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
                                 </svg>
                                 <span className="sr-only">GitHub account</span>
+                            </a>
+                            <span className='border-r border-gray-400 ms-5'></span>
+                            <a href="https://jiang.lemonsqueezy.com/checkout/buy/ad4c8d9a-ece8-471f-9706-cb01408ed625" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M80,56V24a8,8,0,0,1,16,0V56a8,8,0,0,1-16,0Zm40,8a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,120,64Zm32,0a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,152,64Zm96,56v8a40,40,0,0,1-37.51,39.91,96.59,96.59,0,0,1-27,40.09H208a8,8,0,0,1,0,16H32a8,8,0,0,1,0-16H56.54A96.3,96.3,0,0,1,24,136V88a8,8,0,0,1,8-8H208A40,40,0,0,1,248,120ZM200,96H40v40a80.27,80.27,0,0,0,45.12,72h69.76A80.27,80.27,0,0,0,200,136Zm32,24a24,24,0,0,0-16-22.62V136a95.78,95.78,0,0,1-1.2,15A24,24,0,0,0,232,128Z"></path></svg> */}
+                                <span className="">请我喝杯咖啡☕</span>
                             </a>
                             {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
