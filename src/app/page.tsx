0e7daefef481f3fc5b5803f527a232bffe515562
makatadi <38482112+draJiang@users.noreply.github.com>
2024-03-18 22:16:59 +0800

    增加作者信息

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FileUploader from "./FileUploader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
 
       <FileUploader/>
 
