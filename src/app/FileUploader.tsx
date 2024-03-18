a05b162a7f365bc95b639fd44928c7bb0a19a497
makatadi <38482112+draJiang@users.noreply.github.com>
2024-03-19 04:50:07 +0800

    优化交互细节

--- src/app/FileUploader.tsx
+++ src/app/FileUploader.tsx
@@ -115,8 +115,8 @@ function FileUploader() {
         <div className="space-y-4 flex h-screen flex-col items-center">
             <div className='grow'>
 
-                <div className='p-20 flex flex-col items-center'>
-                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className='py-20 px-8 flex flex-col items-center'>
+                    <div className="mx-auto max-w-7xl lg:px-8">
                         <div className="mx-auto max-w-2xl text-center">
                             {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
                             <h1 className="text-4xl font-bold tracking-tight dark:text-gray-300 text-gray-900 sm:text-6xl">
@@ -132,6 +132,7 @@ function FileUploader() {
                         <input
                             ref={hiddenFileInput}
                             type="file"
+                            accept=".html"
                             style={{ display: 'none' }}
                             onChange={handleFileChange}
                         />
@@ -196,7 +197,9 @@ function FileUploader() {
                                 </svg>
                                 <span className="sr-only">GitHub account</span>
                             </a>
-                            <span className='border-r border-gray-400 ms-5'></span>
+
+                            <span className='border-r-2 h-4 border-gray-300 dark:border-gray-700 ms-5'></span>
+
                             <a href="https://jiang.lemonsqueezy.com/checkout/buy/ad4c8d9a-ece8-471f-9706-cb01408ed625" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M80,56V24a8,8,0,0,1,16,0V56a8,8,0,0,1-16,0Zm40,8a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,120,64Zm32,0a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,152,64Zm96,56v8a40,40,0,0,1-37.51,39.91,96.59,96.59,0,0,1-27,40.09H208a8,8,0,0,1,0,16H32a8,8,0,0,1,0-16H56.54A96.3,96.3,0,0,1,24,136V88a8,8,0,0,1,8-8H208A40,40,0,0,1,248,120ZM200,96H40v40a80.27,80.27,0,0,0,45.12,72h69.76A80.27,80.27,0,0,0,200,136Zm32,24a24,24,0,0,0-16-22.62V136a95.78,95.78,0,0,1-1.2,15A24,24,0,0,0,232,128Z"></path></svg> */}
                                 <span className="">请我喝杯咖啡☕</span>
