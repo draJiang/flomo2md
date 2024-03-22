01783e3942aeafcfdaf1a5d7f9471ed36def8ff1
makatadi <38482112+draJiang@users.noreply.github.com>
2024-03-23 01:26:32 +0800

    新增邮件入口

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,6 +84,17 @@ export default function Home() {
                 <span className="sr-only">GitHub account</span>
               </a>
 
+              <a href="mailto:jzlong666@gmail.com?subject=flomo2md：" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                <svg className="" width='18px' aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z" />
+                  <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z" />
+                </svg>
+
+
+
+                <span className="sr-only">Discord community</span>
+              </a>
+
             </div>
           </div>
         </div>
